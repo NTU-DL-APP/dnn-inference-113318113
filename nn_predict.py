c4c5048e060d41f53125fb1924f64686316c5a53
unknown <113318113@cc.ntut.edu.tw>
2025-06-10 13:57:36 +0800

    final answer

--- nn_predict.py
+++ nn_predict.py
@@ -7,9 +7,15 @@ def relu(x):
     return np.maximum(0, x)
 
 def softmax(x):
-    x = x - np.max(x, axis=-1, keepdims=True)
-    e_x = np.exp(x)
-    return e_x / np.sum(e_x, axis=-1, keepdims=True)
+    x = np.array(x)
+    if x.ndim == 1:
+        x = x - np.max(x)
+        e_x = np.exp(x)
+        return e_x / np.sum(e_x)
+    else:
+        x = x - np.max(x, axis=1, keepdims=True)
+        e_x = np.exp(x)
+        return e_x / np.sum(e_x, axis=1, keepdims=True)
 
 # === Flatten ===
 def flatten(x):
